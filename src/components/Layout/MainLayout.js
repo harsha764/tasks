e1c84b22e24995580ce88aa35293eda4c623d71d
harsha764 <hreddy710@gmail.com>
2019-08-12 18:19:56 +0530

    latest code

--- src/components/Layout/MainLayout.js
+++ src/components/Layout/MainLayout.js
@@ -1,5 +1,11 @@
 import { Content, Header, Sidebar } from 'components/Layout';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { MyCompanies, AllCompanies, SingleCompany } from '../Pages/Companies';
+import { AddProcedure, AllProcedures, ViewProcedure, EditProcedure } from '../Pages/Procedures';
+import { CreateForm, EditForm, AllForms, ViewForm } from '../Pages/Forms';
+import CreateUser from "../Pages/Dashboard/CreateUsers";
+import ViewUser from "../Pages/Dashboard/ViewUser";
 
 class MainLayout extends React.Component {
   static isSidebarOpen() {
@@ -59,7 +65,19 @@ class MainLayout extends React.Component {
         <Sidebar />
         <Content fluid onClick={this.handleContentClick}>
           <Header />
-          {children}
+          <Route exact path="/dashboard/create_user" component={CreateUser} />
+          <Route exact path="/dashboard/view_user" component={ViewUser} />
+          <Route exact path="/dashboard/my_companies" component={MyCompanies} />
+          <Route exact path="/dashboard/all_companies" component={AllCompanies} />
+          <Route path='/dashboard/company/:id' render={(props) => <SingleCompany {...props} />} />
+          <Route exact path="/dashboard/add_procedure" component={AddProcedure} />
+          <Route exact path="/dashboard/all_procedures" component={AllProcedures} />
+          <Route path='/dashboard/procedure/:id' render={(props) => <ViewProcedure {...props} />} />
+          <Route path='/dashboard/editprocedure/:id' render={(props) => <EditProcedure {...props} />} />
+          <Route exact path="/dashboard/create_forms" component={CreateForm} />
+          <Route exact path="/dashboard/all_forms" component={AllForms} />
+          <Route exact path="/dashboard/edit_forms/:id" render={(props) => <EditForm {...props} />} />
+          <Route exact path="/dashboard/view_form/:id" render={(props) => <ViewForm {...props} />} />
         </Content>
       </main>
     );
